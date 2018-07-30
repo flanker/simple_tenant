94583b0e863c01f62e2cfec48de6d3f959a11553
FENG Zhichao <flankerfc@gmail.com>
2018-07-30 09:56:00 +0800

    Support tenant field use custom data type.

--- spec/simple_tenant/model_extensions_with_inheritance_spec.rb
+++ spec/simple_tenant/model_extensions_with_inheritance_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'SimpleTenant::ModelExtensions with inheritance' do
 
     field :name, type: String
 
-    tenanted_by :account_id
+    tenanted_by :account_id, type: String
   end
 
   class AModel < BaseModel
@@ -30,11 +30,11 @@ RSpec.describe 'SimpleTenant::ModelExtensions with inheritance' do
     end
 
     it 'sets tenant as current tenant if current tenant is set' do
-      SimpleTenant.current_tenant = 828
+      SimpleTenant.current_tenant = 'ABCD'
 
       document = AModel.new
 
-      expect(document.account_id).to equal(828)
+      expect(document.account_id).to eq('ABCD')
     end
 
   end
@@ -50,11 +50,11 @@ RSpec.describe 'SimpleTenant::ModelExtensions with inheritance' do
     end
 
     it 'saves tenant as current tenant if current tenant is set' do
-      SimpleTenant.current_tenant = 828
+      SimpleTenant.current_tenant = 'ABCD'
 
       document = AModel.create
 
-      expect(document.reload.account_id).to eq(828)
+      expect(document.reload.account_id).to eq('ABCD')
     end
 
   end
@@ -62,8 +62,8 @@ RSpec.describe 'SimpleTenant::ModelExtensions with inheritance' do
   context 'document query' do
 
     before do
-      AModel.create name: 'document with tenant', account_id: 828
-      AModel.create name: 'document with another tenant', account_id: 1113
+      AModel.create name: 'document with tenant', account_id: 'ABCD'
+      AModel.create name: 'document with another tenant', account_id: 'EFG'
       AModel.create name: 'document without tenant'
     end
 
@@ -75,7 +75,7 @@ RSpec.describe 'SimpleTenant::ModelExtensions with inheritance' do
     end
 
     it 'searches document with given tenant if current tenant is set' do
-      SimpleTenant.current_tenant = 828
+      SimpleTenant.current_tenant = 'ABCD'
 
       expect(AModel.count).to eq(1)
       expect(AModel.first.name).to eq('document with tenant')
